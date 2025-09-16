d1f0d5874b2b49e669c2157661b7264528a721f3
Moudi <mahmoud_jaderi@codewithmj.com>
2025-09-16 11:03:43 +1000

    Refactor: Use SCSS variables for consistent styling and remove unused animation provider

--- TodoApp.Client/src/app/app.config.ts
+++ TodoApp.Client/src/app/app.config.ts
@@ -1,12 +1,10 @@
 import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter([]),
-    provideHttpClient(),
-    provideAnimations()
+    provideHttpClient()
   ]
 };
